0563040835541eb7496d939d05db4a2118eb3429
Tahir Ishaq <tahir.ishaq@adevinta.com>
2022-07-18 00:22:46 +0200

    unit test added ''upload' api for saving cars

--- src/test/java/com/move/car/controller/CarControllerTest.java
+++ src/test/java/com/move/car/controller/CarControllerTest.java
@@ -10,17 +10,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @WebMvcTest(CarController.class)
 class CarControllerTest {
@@ -31,12 +36,58 @@ class CarControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Test
-    void shouldCreateListOFCars_afterUploadingCorrectFile() {
+    @Nested
+    class SaveCars {
 
+        @Test
+        void shouldSaveListOfCarsForUploadedFile() throws Exception {
+            // given
+            var correctFile = getFileFor("/idealData_car.csv", "text/csv");
+
+            // then
+            mockMvc.perform(MockMvcRequestBuilders.multipart("/api/csv/upload")
+                            .file(correctFile))
+                            .andExpect(status().isOk());
+        }
+
+        @Test
+        void shouldRetrunBadRequestForBadFormatFile() throws Exception {
+            // given
+            var corruptFile = getFileFor("/badFormat_car.json", "text/json");
+
+            // then
+            mockMvc.perform(MockMvcRequestBuilders.multipart("/api/csv/upload").file(corruptFile))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(result -> assertEquals("{\"message\":\"Please upload a csv file!\"}",
+                            result.getResponse().getContentAsString()));
+        }
+
+        @Test
+        void shouldRetrunExpectationFailedErrorForBrokenDataFile() throws Exception {
+            // given
+            var fileName = "brokenData_car.csv";
+            var corruptFile = getFileFor("/" + fileName, "text/csv");
+
+            // when
+            doThrow(InternalException.class)
+                    .when(carService)
+                    .save(any());
+
+            // then
+            mockMvc.perform(MockMvcRequestBuilders.multipart("/api/csv/upload").file(corruptFile))
+                    .andExpect(status().isExpectationFailed())
+                    .andExpect(result ->
+                            assertEquals("{\"message\":\"Could not upload the file: " + fileName + "!\"}",
+                            result.getResponse().getContentAsString()));
+        }
+
+        private MockMultipartFile getFileFor(String filePath, String contentType) throws IOException {
+            var fileResource = new ClassPathResource(filePath);
+            var file = new MockMultipartFile("file", fileResource.getFilename(), contentType,
+                    fileResource.getInputStream());
+            return file;
+        }
     }
-
-
 
     @Nested
     class GetAllCars {
