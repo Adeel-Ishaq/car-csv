faa6475b1a4a8522033ed417ea2e3f23752e85b6
Tahir Ishaq <tahir.ishaq@adevinta.com>
2022-07-17 12:02:19 +0200

    helpful comments added for file upload exception

--- src/main/java/com/move/car/exception/FileUploadExceptionAdvice.java
+++ src/main/java/com/move/car/exception/FileUploadExceptionAdvice.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+/**
+ * Here we handled the file upload exception case when a request exceeds Max Upload Size. The system will throw
+ * `MaxUploadSizeExceededException` and we will use `@ControllerAdvice` with `@ExceptionHandler` annotation
+ * for handling the exceptions.
+ */
 @ControllerAdvice
 public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(MaxUploadSizeExceededException.class)
